0ca578dc7183b8184b21466af041a68fc841b5f5
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-05-21 21:37:52 +0700

    test:setup pytest

---
+++ tests/test_parse.py
@@ -0,0 +1,14 @@
+from kundi.parse import parse_export, parse_import
+
+sample_input = "word1 def1\nword2 def2\nword3 def3"
+listresult = parse_import(sample_input, " ", "\n")
+stringresult = parse_export(listresult, " ", "\n")
+
+
+def test_import():
+    assert listresult == [{'word': 'word1', 'definition': 'def1'}, {
+        'word': 'word2', 'definition': 'def2'}, {'word': 'word3', 'definition': 'def3'}]
+
+
+def test_export():
+    assert stringresult == "word1 def1\nword2 def2\nword3 def3"
