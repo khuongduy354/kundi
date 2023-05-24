701de83f2d54b2dc09165f2b6b65795d1acc9936
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-05-24 08:10:06 +0700

    feat: firebase setup

--- kundi/server/main.py
+++ kundi/server/main.py
@@ -1,15 +1,73 @@
-from typing import Union
+from typing import Callable, Union, Annotated, List
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import auth
+from firebase_admin import credentials
 
-from fastapi import FastAPI
+# auth setup
+cred = credentials.Certificate("private_key.json")
+firebase = firebase_admin.initialize_app(cred)
+user: auth.UserRecord = auth.get_user("YJEuD5XQSSe4O7sWrNawhaNfiZV2")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class Card(BaseModel):
+    card_id: int
+    word: str
+    definition: str
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+# Auth
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/v1/login")
+def login_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    isAuth = auth.verify_id_token(token, check_revoked=True)
+    if isAuth:
+        return {"name": "someone"}
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    # Cards
+
+
+@app.get("/v1/cards/{card_id}")
+def get_cards(card_id: str):
+    pass
+
+
+@app.post("/v1/cards/")
+def create_cards(cards: List[Card]):
+    pass
+
+
+@app.put("/v1/cards/{card_id}")
+def update_card(card_id: int, new_card: Card):
+    pass
+
+
+@app.delete("/v1/cards/{card_id}")
+def delete_card(card_id: int):
+
+    pass
+
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
--- readme.md
+++ readme.md
@@ -26,4 +26,30 @@ https://pythonspeed.com/articles/faster-text-processing/
 python easier but rust faster 
 https://www.reddit.com/r/rust/comments/dto1ew/rust_vs_other_for_text_processing/
 https://stackoverflow.com/questions/76257409/rust-regex-performance-in-comparison-with-python
-https://stackoverflow.com/questions/41390244/rust-slower-than-python-at-parsing-files
+https://stackoverflow.com/questions/41390244/rust-slower-than-python-at-parsing-files 
+
+- dependency injection  
+https://fastapi.tiangolo.com/tutorial/dependencies/#import-depends  
+python Depends accepts a callable
+ 
+- Auth   
+https://fastapi.tiangolo.com/tutorial/security/first-steps/ 
+when use Depends OAuth in request -> FastAPI handle OAuth automatically 
+
+- Middlewares and D.I
+Dependency can has its own dependencies -> chain pretty much like Middlewares
+https://stackoverflow.com/questions/66632841/fastapi-dependency-vs-middleware#:~:text=Dependency%3A%20you%20use%20it%20to,the%20request%20to%20your%20logic. 
+-> few similar use cases
+-> use D.I if handle request before route handler 
+
+- Setup with firebase 
+https://pythonalgos.com/python-firebase-authentication-integration-with-fastapi/ 
+pyrebase 
+
+- PIP SUCKS 
+firebase-admin take hours to install, 
+but after i run pip3 install --upgrade pip
+it install like instantly
+
+# Credentials 
+firebase appsecret = kundi 
